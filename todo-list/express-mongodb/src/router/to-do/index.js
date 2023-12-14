014fa77461ab8b9cd78503e76b78ca41d0e168eb
nicholay <nicholaysheremet@gmail.com>
2023-12-14 23:42:16 +0300

    primitive get API

--- todo-list/express-mongodb/src/router/to-do/index.js
+++ todo-list/express-mongodb/src/router/to-do/index.js
@@ -1,23 +1,39 @@
 import { Router } from "express";
 import dao from "../../database";
 
-const { Meetups, ToDo } = dao.models;
+const { ToDo } = dao.models;
 
 const toDoRouter = Router();
 
+toDoRouter.get("/create", async (req, res) => {
+  const { data } = req.body;
+  const testToDo = {
+    label: `Test ToDo ${Date.now()}`,
+    finished: false,
+  };
+  const toDo = await ToDo.create(testToDo);
+  res.json({ success: 1, data: toDo})
+});
+
 toDoRouter.get("/", async (req, res) => {
-  const meetups = await Meetups.find();
-  console.log(meetups);
-  
-  res.send("Get list of to-do");
+  const toDoes = await ToDo.find();
+  res.json({ success: 1, data: toDoes})
 });
 
-toDoRouter.get("/delete", (req, res) => {
-  res.send("Delete to-do from list");
+toDoRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const toDo = await ToDo.findById(id);
+  res.json({ success: 1, data: toDo})
 });
 
-toDoRouter.get("/update", (req, res) => {
+toDoRouter.get("/:id/update", (req, res) => {
+  const { id } = req.params;
   res.send("Update to-do in list");
+});
+
+toDoRouter.get("/:id/delete", (req, res) => {
+  const { id } = req.params;
+  res.send("Delete to-do from list");
 });
 
 export default toDoRouter;
